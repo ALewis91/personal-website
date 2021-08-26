6542d1ec74c5019124d57a578711362d5287aaae
Aaron Lewis <aaron11125@gmail.com>
2021-08-26 16:45:06 -0700

    Updated ExperienceList component to process experience list from props directly rather than extracting from parent and converted ExperienceList to functional component

--- src/components/Experience/ExperienceList/ExperienceList.js
+++ src/components/Experience/ExperienceList/ExperienceList.js
@@ -1,20 +1,17 @@
-import { Component } from "react";
 import ExperienceItem from "../ExperienceItem/ExperienceItem";
 import classes from "./ExperienceList.module.css";
 
-class ExperienceList extends Component {
-  render() {
-    let experienceList = [];
-    if (this.props.experiences) {
-      const parent = this.props.experiences;
-      for (const exp of parent.experiences) {
-        experienceList.push(
-          <ExperienceItem key={exp.id} experience={exp}></ExperienceItem>
-        );
-      }
+const ExperienceList = (props) => {
+  let experienceList = [];
+
+  if (props.experiences) {
+    for (const exp of props.experiences) {
+      experienceList.push(
+        <ExperienceItem key={exp.id} experience={exp}></ExperienceItem>
+      );
     }
-    return <ul className={classes.experienceList}>{experienceList}</ul>;
   }
-}
+  return <ul className={classes.experience_list}>{experienceList}</ul>;
+};
 
 export default ExperienceList;
