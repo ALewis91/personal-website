679eb10ab10e16c4a691992089c1eade2648fea8
Aaron Lewis <aaron11125@gmail.com>
2021-09-01 03:32:27 -0700

    Updated Button component to take css module classes as props for more dynamic styling

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,12 @@ import classes from "./Button.module.css";
 
 function Button(props) {
   return (
-    <div className={classes.Button} onClick={props.onClick}>
+    <div
+      className={
+        props.classes ? classes.Button + " " + props.classes : classes.Button
+      }
+      onClick={props.onClick}
+    >
       {props.children}
     </div>
   );
